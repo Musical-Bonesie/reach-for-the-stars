677f4ba08a5a49cb43594b951b1dd38b2632735f
Musical-Bonesie <83307690+Musical-Bonesie@users.noreply.github.com>
2021-09-15 15:24:50 -0600

    added loading img for APOD page

--- src/pages/Stars/Stars.js
+++ src/pages/Stars/Stars.js
@@ -1,22 +1,51 @@
-import Placeholder from "../../assets/images/placeholder-stars.jpg";
+import { Component } from "react";
+import { getAllImages } from "../../utils/dataUtils";
 import "./Stars.scss";
 //TODO remember to add photo alt {}
-export default function Stars() {
-  return (
-    <>
-      <h1>Stars page</h1>
-      <section className="card">
-        <figure className="card__figure">
-          <img className="card__image" src={Placeholder} alt="description" />
-        </figure>
-        <article className="card__header">
-          <h1 className="card__title">Star Title</h1>
-          <p>lorem ipsum dolor sit amet, </p>
-        </article>
-        <div className="card__button">
-          <span>Like</span>
-        </div>
-      </section>
-    </>
-  );
+
+export default class Stars extends Component {
+  state = {
+    APOD: null,
+  };
+
+  componentDidMount() {
+    getAllImages()
+      .then((res) => {
+        this.setState({ APOD: res.data });
+      })
+      .catch((error) => {
+        console.log("error in didmount", error);
+      });
+  }
+
+  render() {
+    console.log("APOD:", this.state.APOD);
+    const APOD = this.state.APOD;
+    if (APOD === null) {
+      return (
+        <>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif" />
+        </>
+      );
+    }
+    return (
+      // this.state.APOD && (
+      <>
+        <h1>Astronomy Picture of the Day</h1>
+        <section className="card">
+          <figure className="card__figure">
+            <img className="card__image" src={APOD.hdurl} alt="description" />
+          </figure>
+          <article className="card__header">
+            <h1 className="card__title">{APOD.title}</h1>
+            <p>{APOD.explanation}</p>
+          </article>
+          <div className="card__button">
+            <button>Like</button>
+          </div>
+        </section>
+      </>
+      // )
+    );
+  }
 }
--- src/utils/dataUtils.js
+++ src/utils/dataUtils.js
@@ -1,6 +1,9 @@
 import axios from "axios";
-export const URL = process.env.REACT_APP_API_URL;
-export const API_KEY = process.env.REACT_APP_API_KEY;
+import { API_KEY } from "./api_key";
+
+require("dotenv").config();
+const URL = `https://api.nasa.gov/planetary/apod?api_key=`;
+// export const API_KEY = process.env.REACT_APP_API_KEY;
 
 //GET all NASA APOD Images
 export const getAllImages = () => axios.get(`${URL}${API_KEY}`);
