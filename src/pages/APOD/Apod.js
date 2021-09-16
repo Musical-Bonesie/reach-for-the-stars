28bd15721e55efd9288f40c4b17471fd0ccf0307
Musical-Bonesie <83307690+Musical-Bonesie@users.noreply.github.com>
2021-09-15 20:25:40 -0600

    fixed bug with shooting star background

--- src/pages/APOD/Apod.js
+++ src/pages/APOD/Apod.js
@@ -7,6 +7,7 @@ import ShootingStars from "../../components/ShootingStars/ShootingStars";
 export default class Stars extends Component {
   state = {
     APOD: null,
+    liked: false,
   };
 
   componentDidMount() {
@@ -18,8 +19,14 @@ export default class Stars extends Component {
         console.log("error in didmount", error);
       });
   }
-
+  handleClick = (event) => {
+    // event.preventDefault()
+    console.log(event);
+    this.setState({ liked: !this.state.liked });
+  };
   render() {
+    const text = this.state.liked ? "like" : "liked";
+    console.log(text);
     console.log("APOD:", this.state.APOD);
     const APOD = this.state.APOD;
     if (APOD === null) {
@@ -35,22 +42,26 @@ export default class Stars extends Component {
     }
     return (
       <>
-        <section className="card">
-          <h1 className="card__heading">Astronomy Picture of the Day</h1>
+        <div>
+          <ShootingStars />
+        </div>
+        <div>
+          <section className="card">
+            <h1 className="card__heading">Astronomy Picture of the Day</h1>
 
-          <figure className="card__figure">
-            <img className="card__image" src={APOD.hdurl} alt={APOD.title} />
-          </figure>
-          <article className="card__header">
-            <h1 className="card__content">{APOD.title}</h1>
-            <h2 className="card__copy">{APOD.date}</h2>
-            <p className="card__copy">{APOD.explanation}</p>
-          </article>
-          <div className="card__button">
-            <button onClick={console.log("click!")}>Like</button>
-          </div>
-        </section>
-        <ShootingStars />
+            <figure className="card__figure">
+              <img className="card__image" src={APOD.hdurl} alt={APOD.title} />
+            </figure>
+            <article className="card__header">
+              <h1 className="card__content">{APOD.title}</h1>
+              <h2 className="card__copy">{APOD.date}</h2>
+              <p className="card__copy">{APOD.explanation}</p>
+            </article>
+            <div className="card__button">
+              <button onClick={this.handleClick}>{text}</button>
+            </div>
+          </section>
+        </div>
       </>
     );
   }
